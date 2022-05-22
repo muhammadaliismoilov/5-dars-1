dc762d4d716c8d54b2d0352f5045ca9104d0c8ab
Пользователь <rustammuborakmuhammdali@gmail.com>
2022-05-22 09:39:42 +0500

    5 dars masalasi

---
+++ js/script.js
@@ -0,0 +1,51 @@
+// let age = Number(prompt("Yoshinggizni kiriting : "));
+
+
+// if(age >= 18){
+
+//     console.log("Siz guvhnoma olsanggiz boladi");
+
+// }
+// else {
+
+//     const year = 18 - age;
+//     console.log(`Sizga ${year} bor`);
+
+// }
+
+// let age = Number(prompt("Yilinngizni kiriting : "));
+
+// console.log(Math.floor(age / 100) + 1);
+
+
+// let x = Number(prompt("X sonini kiriting "));
+// let y = Number(prompt("Y sonini kiriting "));
+
+// let z = Number(prompt(`${x} + ${y} = ?`));
+// if(x + y == z){
+//     console.log("Togri");
+// }
+// else{
+//     console.log("Notogri");
+// }
+
+
+// 1-masala
+
+let bor = Number(prompt("Pul kiriting (so`mda) = "));
+
+let dolr = 10897;
+let yevro = 12248;
+
+let hotel = 250 * dolr;
+    bilet = 500 * dolr;
+    muzey = 120 * yevro;
+
+sum = bilet + hotel + muzey;
+console.log(sum);
+
+if (sum <= bor) {
+    console.log("Oq yo`l Alisher");
+} else{
+    console.log("Alisher, ozgina sabr qilish kerak bo’lar ekan");
+}
